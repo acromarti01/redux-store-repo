d80a854074f032ac5192ba4ce51d577d0762cec2
Anthony Cromartie <acromarti@gmail.com>
2021-10-24 23:05:12 -0500

    moved the inital state object from GlobalState.js to reducers.js

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -5,13 +5,6 @@ const StoreContext = createContext();
 const { Provider } = StoreContext;
 
 const StoreProvider = ({ value = [], ...props }) => {
-  const [state, dispatch] = useProductReducer({
-    products: [],
-    cart: [],
-    cartOpen: false,
-    categories: [],
-    currentCategory: '',
-  });
 
   return <Provider value={[state, dispatch]} {...props} />;
 };
