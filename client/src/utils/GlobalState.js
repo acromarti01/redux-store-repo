ecd91656c162de782433c1e4605d8d8e95251c15
Anthony Cromartie <acromarti@gmail.com>
2021-10-24 23:22:41 -0500

    created StoreProvider that returns the provider provided by redux

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,3 +1,9 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from "./store/store";
+
+const StoreProvider = ({ value = [], ...props }) => {
+  return <Provider store={store} {...props} />
+};
+
+export default StoreProvider;
